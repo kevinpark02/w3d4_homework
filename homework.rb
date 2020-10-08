7389ec21c1798f332fdbf11fb6a425b150f7b42d
kevin park <kevin.park02@gmail.com>
2020-10-08 08:07:41 -0700

    "Make these changes for me please"

---
+++ homework.rb
@@ -0,0 +1,6 @@
+def print_name(name)
+    puts name
+end
+
+p "Does doing this change anything?"
+p "ok what about now?"
---
+++ sample_file_2.rb
@@ -0,0 +1,5 @@
+def print_msg
+    puts "I created this file for Adding a single file to git"
+end
+
+"ok so if I make this change and type git add ., it should show red letters"
